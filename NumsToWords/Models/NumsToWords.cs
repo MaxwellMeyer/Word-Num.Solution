f91f3b40173e5ad9eb496c3e59caf869e77cf751
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-15 14:19:22 -0700

    pass test for NumToWord method
    Co-Authored-By: Allister <allisterkmoon@gmail.com>

--- NumsToWords/Models/NumsToWords.cs
+++ NumsToWords/Models/NumsToWords.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collection.Generic;
+using System.Collections.Generic;
 
 namespace NumWord
 {
@@ -7,7 +7,12 @@ namespace NumWord
   {
     public static Dictionary<int, string> numWords = new Dictionary<int, string>()
     {
-      {1,"one"}, {2,"two"}, {3,"three"}, {4,"four"}, {5,"five"}, {6,"six"}, {6,"ONE"}, {7,"seven"}, {8,"eight"}, {9,"nine"}
+      {1,"one"}, {2,"two"}, {3,"three"}, {4,"four"}, {5,"five"}, {6,"six"}, {7,"seven"}, {8,"eight"}, {9,"nine"}
     };
+
+    public static string NumToWord(int num)
+    {
+      return numWords[num];
+    }
   }
 }
