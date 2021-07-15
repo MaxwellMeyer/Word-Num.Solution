390437bd4f4d87887b07ff13330e59a606fda67e
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-15 13:47:21 -0700

    set up project file structure, add numWords dictionary to Converter class
    Co-Authored-By: Allister <allisterkmoon@gmail.com>

---
+++ NumsToWords/Models/NumsToWords.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collection.Generic;
+
+namespace NumWord
+{
+  public class Converter
+  {
+    public static Dictionary<int, string> numWords = new Dictionary<int, string>()
+    {
+      {1,"one"}, {2,"two"}, {3,"three"}, {4,"four"}, {5,"five"}, {6,"six"}, {6,"ONE"}, {7,"seven"}, {8,"eight"}, {9,"nine"}
+    };
+  }
+}
